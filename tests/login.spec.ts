3338d69343baf9b53d1bef5452325af6d6a33ef7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2026-05-01 09:43:12 +0100

    Refactor login steps into a helper function

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import {test, expect, type Page} from '@playwright/test';
 
 const URL = 'https://www.saucedemo.com/';
 
@@ -6,27 +6,27 @@ test.beforeEach(async ({ page }, testInfo ) => {
   await page.goto(URL);
 });
 
+async function login(page:Page, username:string, password:string) {
+  await page.getByRole('textbox', { name: 'Username' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.getByRole('button', { name: 'Login' }).click()
+}
+
 test.describe('Homepage - Login', ()=>{
   test('should be successfull if credentials are valid', async ({ page }) => {
-    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
-    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await login(page, 'standard_user', 'secret_sauce');
     await expect(page).toHaveURL(/.*inventory/);  
   });
 
   test('should fail if username is invalid', async ({ page }) => {
-    await page.getByRole('textbox', { name: 'Username' }).fill('invalid_user');
-    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await login(page, 'invalid_user', 'secret_sauce');
     const loginError = page.locator('[data-test="error"]');
     await expect(loginError).toBeVisible();
     await expect(loginError).toContainText('Epic sadface: Username and password do not match any user in this service');
   });
 
   test('should fail if password is invalid', async ({ page }) => {
-    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
-    await page.getByRole('textbox', { name: 'Password' }).fill('invalid_password');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await login(page, 'standard_user', 'invalid_password');
     const loginError = page.locator('[data-test="error"]');
     await expect(loginError).toBeVisible();
     await expect(loginError).toContainText('Epic sadface: Username and password do not match any user in this service');
