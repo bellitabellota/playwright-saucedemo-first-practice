507c14a2f24c33c709b19c3b20343869bfd8dbf9
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2026-05-01 09:17:04 +0100

    Add test cases for Login

---
+++ tests/login.spec.ts
@@ -0,0 +1,36 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Homepage - Login', ()=>{
+  test('should be successfull if credentials are valid', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL(/.*inventory/);  
+  });
+
+  test('should fail if username is invalid', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', { name: 'Username' }).fill('invalid_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    const loginError = page.locator('[data-test="error"]');
+    await expect(loginError).toBeVisible();
+    await expect(loginError).toContainText('Epic sadface: Username and password do not match any user in this service');
+  });
+
+  test('should fail if password is invalid', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('invalid_password');
+    await page.getByRole('button', { name: 'Login' }).click();
+    const loginError = page.locator('[data-test="error"]');
+    await expect(loginError).toBeVisible();
+    await expect(loginError).toContainText('Epic sadface: Username and password do not match any user in this service');
+  });
+});
+
+
+
+
+
