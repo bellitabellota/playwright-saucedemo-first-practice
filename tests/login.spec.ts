1dbcc93c913a328a6a4c49a6dfcd7ae57f7eca2b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2026-05-01 15:05:41 +0100

    Make use of the LoginPage Object Model

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,29 @@
 import {test, expect, type Page} from '@playwright/test';
+import { LoginPage } from '../pages/login-page';
 
 const URL = 'https://www.saucedemo.com/';
+let loginPage: LoginPage;
 
 test.beforeEach(async ({ page }, testInfo ) => {
   await page.goto(URL);
+  loginPage = new LoginPage(page);
 });
-
-async function login(page:Page, username:string, password:string) {
-  await page.getByRole('textbox', { name: 'Username' }).fill(username);
-  await page.getByRole('textbox', { name: 'Password' }).fill(password);
-  await page.getByRole('button', { name: 'Login' }).click()
-}
 
 test.describe('Homepage - Login', ()=>{
   test('should be successfull if credentials are valid', async ({ page }) => {
-    await login(page, 'standard_user', 'secret_sauce');
+    await loginPage.login('standard_user', 'secret_sauce');
     await expect(page).toHaveURL(/.*inventory/);  
   });
 
   test('should fail if username is invalid', async ({ page }) => {
-    await login(page, 'invalid_user', 'secret_sauce');
+    await loginPage.login('invalid_user', 'secret_sauce');
     const loginError = page.locator('[data-test="error"]');
     await expect(loginError).toBeVisible();
     await expect(loginError).toContainText('Epic sadface: Username and password do not match any user in this service');
   });
 
   test('should fail if password is invalid', async ({ page }) => {
-    await login(page, 'standard_user', 'invalid_password');
+    await loginPage.login('standard_user', 'invalid_password');
     const loginError = page.locator('[data-test="error"]');
     await expect(loginError).toBeVisible();
     await expect(loginError).toContainText('Epic sadface: Username and password do not match any user in this service');
