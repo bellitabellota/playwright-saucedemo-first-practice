71a9ca1026809252f9f98444b32fb6cc2ee58eb7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2026-05-02 08:33:21 +0100

    Clear cookies before each test to ensure initial state

--- tests/purchase/cart.spec.ts
+++ tests/purchase/cart.spec.ts
@@ -6,7 +6,17 @@ const URL = 'https://www.saucedemo.com/';
 test.describe('Cart functionality', () => {
   let loginPage: LoginPage;
   
-    test.beforeEach(async ({ page } ) => {
+    test.beforeEach(async ({ context, page } ) => {
+      /*
+      - Do not assert that initial state is as desired, rather control the enviroment and guarantee this.
+      - The site sourcedemo.com stores the cart in the browser session and can be reset to guarantee cart is empty before tests run.
+      - If you are unsure how the application handles the cart state you can inspect the localStorage/sessionStorage in the 'Application' tab of DevTools, and check under the 'Network' tab if upon adding an item to the cart a POST or PUT request is sent. 
+      Furthermore, 
+          - if the cart persists after refresh without login --> likely localStorage
+          - if cart persists across devices --> backend
+          - if you see API calls when adding item --> backend
+       */
+      await context.clearCookies();
       await page.goto(URL);
       loginPage = new LoginPage(page);
       await loginPage.login('standard_user', 'secret_sauce');
